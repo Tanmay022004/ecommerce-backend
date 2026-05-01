19123dedd410793d826da0c2685402defd6c48cf
Tanmay Gedam <tanmaygedam111@gmail.com>
2026-05-01 12:57:48 +0530

    added routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require("../Models/Product");
+const Product = require("../models/Product");
 
 exports.getProducts = async (req, res) => {
   const { keyword, category } = req.query;
