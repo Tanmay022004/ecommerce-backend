d937b68bc34fc2c04405431fce5c30d75d0deb3d
Tanmay Gedam <tanmaygedam111@gmail.com>
2026-05-01 12:26:42 +0530

    added routes

--- server.js
+++ server.js
@@ -18,16 +18,16 @@ app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-    
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/cart", require("./routes/cartRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes"));
-app.use("/api/admin", require("./routes/adminRoutes"));
+
+app.use("/api/auth", require("./Routes/authRoutes"));
+app.use("/api/products", require("./Routes/productRoutes"));
+app.use("/api/cart", require("./Routes/cartRoutes"));
+app.use("/api/orders", require("./Routes/orderRoutes"));
+app.use("/api/admin", require("./Routes/adminRoutes"));
 
 app.listen(5000, () => console.log("Server running on 5000"));
 
-app.use("/api/payment", require("./routes/paymentRoutes"));
+app.use("/api/payment", require("./Routes/paymentRoutes"));
 
-app.use("/api/admin", require("./routes/adminRoutes"));
+app.use("/api/admin", require("./Routes/adminRoutes"));
 
